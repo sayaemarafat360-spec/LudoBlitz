26eb3b797ebc379b6c83c20e865fb7814b190f7c
Sayaem Arafat <sayaemarafat360@gmail.com>
2026-03-26 17:37:24 +0600

    gjhgggdg

--- app/src/main/java/com/ludoblitz/app/ui/screens/LeaderboardScreen.kt
+++ app/src/main/java/com/ludoblitz/app/ui/screens/LeaderboardScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
